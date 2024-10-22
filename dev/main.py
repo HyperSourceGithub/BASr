c908ef542758f13a3ce14069b9d1064f1460a51e
HyperSourceGithub <lucian@raileanu.ca>
2024-10-22 09:47:27 -0400

    Minor changes, added user input and began work on some bugs

--- dev/main.py
+++ dev/main.py
@@ -21,8 +21,15 @@ def convert_base(num1, b1, b2):
     return from_base_10(base_10_num, b2)
 
 # Example usage:
+print("1011(2) to b10")
 print(convert_base("1011", 2, 10))  # Output: 11
 print(convert_base("A2", 16, 2))    # Output: 10100010
 print(convert_base("19", 10, 2))    # Output: 10011
 print(convert_base("101", 2, 16))   # Output: 5
 print(convert_base("123", 5, 10))   # Output: Invalid number '123' for base 5
+
+
+number = int(input("Enter \#: "))
+base1 = int(input("Enter b1: "))
+base2 = int(input("Enter b2: "))
+print(convert_base(number, base1, base2))
